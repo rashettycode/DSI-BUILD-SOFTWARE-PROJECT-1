eb07bd0434f425bad331ae8f28a05b4a94148ab6
RaShe1 <rahul.shetty0202@gmail.com>
2024-01-17 19:31:06 -0500

    Update setup.py
    error correction

--- assignmentpkg/setup.py
+++ assignmentpkg/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(name='assignmentpkg',
       version='2.0.0',author='Kevin, Ashish, Rahul',
       packages=['assignmentpkg'],
-      install_requires=['requests', 'numpy', 'pandas', 'matplotlib', 'yaml']
+      install_requires=['requests', 'numpy', 'pandas', 'matplotlib', 'yaml'])
 
