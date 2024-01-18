898b41223419c17f9cab3629ec10f8f050ac9852
RaShe1 <rahul.shetty0202@gmail.com>
2024-01-17 19:09:17 -0500

    Update setup.py
    minor edits to assignmentpkg

--- assignmentpkg/setup.py
+++ assignmentpkg/setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
-setup(name='assignment',
+setup(name='assignmentpkg',
       version='2.0.0',author='Kevin,Ashish,Rahul',
       packages=['assignmentpkg'],
-      install_requires=['requests''numpy','pandas','matplotlib','yaml'])
+      install_requires=['requests', 'numpy', 'pandas', 'matplotlib', 'yaml']
+
